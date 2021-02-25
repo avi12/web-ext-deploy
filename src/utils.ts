6ccc9e6921eb7c586b7720ba4ef356bef3dc1f47
avi12 <avi6106@gmail.com>
2021-02-25 16:28:37 +0200

    utils: Extracted "getEvaluation()", "clearInput()", "getVerboseMessage()"

--- src/utils.ts
+++ src/utils.ts
@@ -29,3 +29,39 @@ export async function disableImages(page: Page) {
     request.continue();
   });
 }
+
+export async function getEvaluation(
+  page: Page,
+  selectors: string[]
+): Promise<string> {
+  const promises = selectors.map(selector => page.waitForSelector(selector));
+  const {
+    // @ts-ignore
+    _remoteObject: { description }
+  } = await Promise.race(promises);
+  return description;
+}
+
+export async function clearInput(page: Page, selector: string) {
+  const elInput = await page.$(selector);
+  await elInput.click();
+  await elInput.focus();
+  await elInput.click({ clickCount: 3 });
+  await elInput.press("Backspace");
+}
+
+export function getVerboseMessage({
+  message,
+  prefix = "Info",
+  store
+}: {
+  message: string;
+  prefix?: string;
+  store: string;
+}): string {
+  const msg = `${prefix} ${store}: ${message}`;
+  if (prefix === "") {
+    return msg.trim();
+  }
+  return msg;
+}
