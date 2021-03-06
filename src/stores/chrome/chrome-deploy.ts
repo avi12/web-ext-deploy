92c690a1bf95480c252291d4faae9f2db20a3ed0
avi12 <avi6106@gmail.com>
2021-03-06 06:43:05 +0200

    Chrome deploy: Fixed code

--- src/stores/chrome/chrome-deploy.ts
+++ src/stores/chrome/chrome-deploy.ts
@@ -1,25 +1,44 @@
 import ChromeUpload from "chrome-webstore-upload";
 import { ChromeOptions } from "./chrome-input";
 import * as fs from "fs";
+import { getVerboseMessage } from "../../utils";
+
+const store = "Chrome";
 
 export async function deployToChrome(options: ChromeOptions) {
-  const client = ChromeUpload({
-    extensionId: options.extId,
-    clientId: options.clientId,
-    clientSecret: options.clientSecret,
-    refreshToken: options.refreshToken
+  return new Promise(async (resolve, reject) => {
+    const client = ChromeUpload({
+      extensionId: options.extId,
+      clientId: options.clientId,
+      clientSecret: options.clientSecret,
+      refreshToken: options.refreshToken
+    });
+
+    if (options.verbose) {
+      console.log(
+        getVerboseMessage({
+          store,
+          message: `Updating extension with ID ${options.extId}`
+        })
+      );
+    }
+
+    const { uploadState, itemError } = await client.uploadExisting(
+      fs.createReadStream(options.zip)
+    );
+    if (uploadState === "FAILURE") {
+      const errors = itemError.map(({ error_detail }) => error_detail);
+      reject(
+        getVerboseMessage({
+          store,
+          message: `Item "${options.extId}":
+          ${errors.join("\n")}`,
+          prefix: "Error"
+        })
+      );
+      return;
+    }
+
+    resolve(true);
   });
-
-  if (options.verbose) {
-    console.log("Uploading...");
-  }
-
-  try {
-    await client.uploadExisting(fs.createReadStream(options.zip));
-  } catch (e) {
-    throw new Error(`Chrome: ${e}`);
-  }
-
-  console.log("Uploaded to Chrome Web Store");
-  return true;
 }
