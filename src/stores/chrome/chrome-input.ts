a4d9030fae2fa110fe65329f20f119ec18d2d069
Avi <avi6106@gmail.com>
2026-02-23 00:24:19 +0200

    Clarified `skipReview` description in `chrome-input.ts` to indicate an attempt at publishing without review.

--- src/stores/chrome/chrome-input.ts
+++ src/stores/chrome/chrome-input.ts
@@ -13,7 +13,7 @@ export const ChromeOptionsSchema = z
     publisherId: z.string().nonempty().describe("Get it from https://github.com/avi12/web-ext-deploy/blob/main/CHROME_WEB_STORE_API.md"),
     refreshToken: z.string().nonempty().describe("Get it from: web-ext-deploy chrome-token --client-id CLIENT_ID --client-secret CLIENT_SECRET"),
     zip: z.string().nonempty().describe(`Path to the ZIP file. Supports "{version}" which is retrieved from package.json`),
-    skipReview: z.boolean().optional().default(false).describe("Publish without waiting for a review"),
+    skipReview: z.boolean().optional().default(false).describe("Attempt to publish without waiting for a review"),
     deployPercentage: z.number().int().min(1).max(100).optional().describe("Staged rollout percentage (1–100) (default: 100)")
   })
   .check(ctx => {
