bceb6159c2359b267b67b18ac237015157215eb6
Avi <avi6106@gmail.com>
2026-02-23 18:33:19 +0200

    Updated `refreshToken` description in `chrome-input.ts` to reference centralized documentation for clarity.

--- src/stores/chrome/chrome-input.ts
+++ src/stores/chrome/chrome-input.ts
@@ -11,7 +11,7 @@ export const ChromeOptionsSchema = z
   .object({
     extId: z.string().nonempty().describe("Get it from https://chromewebstore.google.com/detail/EXT_ID"),
     publisherId: z.string().nonempty().describe("Get it from https://github.com/avi12/web-ext-deploy/blob/main/CHROME_WEB_STORE_API.md"),
-    refreshToken: z.string().nonempty().describe("Get it from: web-ext-deploy chrome-token --client-id CLIENT_ID --client-secret CLIENT_SECRET"),
+    refreshToken: z.string().nonempty().describe("Follow https://github.com/avi12/web-ext-deploy/blob/main/CHROME_WEB_STORE_API.md"),
     zip: z.string().nonempty().describe(`Path to the ZIP file. Supports "{version}" which is retrieved from package.json`),
     skipReview: z.boolean().optional().default(false).describe("Attempt to publish without waiting for a review"),
     deployPercentage: z.number().int().min(1).max(100).optional().describe("Staged rollout percentage (1–100) (default: 100)")
